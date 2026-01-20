077dbd0fb5c47d53e8ef7397ea37d67b301eb8ac
Hwaeng <hwaengineers@outlook.com>
2026-01-20 13:07:16 +0000

    new updates to the commit message. Lines starting

---
+++ javascripts/mmenu.js
@@ -0,0 +1,6 @@
+// Make Mmenu available globally from CDN
+// The Mmenu library is loaded via CDN: https://cdn.jsdelivr.net/npm/mmenu-js@9.3.0/dist/mmenu.js
+// This module just ensures it's available when imported
+
+export default window.Mmenu || {};
+
